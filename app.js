b63fa1b3e22df9a071a27417e1dbf3a1c9202c66
Nguyễn Anh Tuấn <45095135+1660689@users.noreply.github.com>
2019-06-05 23:44:12 +0700

    Cập nhật

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Xin chao');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req,res) =>{
+    res.render('home');
+})
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'price_main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/pricing', (req,res) =>{
+    // res.end('hello express');
+    res.render('price_home');
+})
+
+var port = 3000;
+app.listen(port, () => {
+    console.log(`server is running at port ${port}`);
+});
